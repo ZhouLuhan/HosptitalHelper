a622d8b377a16d1ef451e381839185f78627fc54
Albert <st.krwlng@gmail.com>
2012-07-07 23:13:48 +0800

    Edit Database

--- HospitalHelper/NewFollowUpTest.aspx.cs
+++ HospitalHelper/NewFollowUpTest.aspx.cs
@@ -9,6 +9,8 @@ namespace HospitalHelper
 {
     public partial class NewFollowUpTest : System.Web.UI.Page
     {
+        private int fid = 0;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
