91aa4606b4346fc5ad59c8e6dd08ec3fbb99e599
Albert <st.krwlng@gmail.com>
2012-07-11 13:10:51 +0800

    Solve one postback problem

--- HospitalHelper/NewFollowUpTest.aspx.cs
+++ HospitalHelper/NewFollowUpTest.aspx.cs
@@ -20,19 +20,13 @@ namespace HospitalHelper
         protected void Page_Load(object sender, EventArgs e)
         {
             string strcon = System.Configuration.ConfigurationManager.ConnectionStrings["HospitalData"].ConnectionString;
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < count+1-1; i++)
             {
                 droptype[i] = new DropDownList();
                 grid[i] = new GridView();
-                
+
                 form1.Controls.Add(droptype[i]);
                 form1.Controls.Add(grid[i]);
-
-                if (!IsPostBack)
-                {
-                    droptype[i].Visible = false;
-                    grid[i].Visible = false;
-                }
 
                 droptype[i].SelectedIndexChanged += new EventHandler(DropDownList1_SelectedIndexChanged);
                 droptype[i].AutoPostBack = true;
@@ -47,7 +41,7 @@ namespace HospitalHelper
                 source[i] = new SqlDataSource(strcon, "SELECT * FROM TESTITEM WHERE TYPE=" + Convert.ToInt32(droptype[i].SelectedValue));
 
                 grid[i].DataSource = source[i];
-                grid[i].DataBind();
+                //grid[i].DataBind();
                 grid[i].AutoGenerateColumns = false;
                 grid[i].Columns.Clear();
 
@@ -72,44 +66,68 @@ namespace HospitalHelper
                 bresult.ItemTemplate = template;
                 grid[i].Columns.Add(bresult);
 
-                //BoundField bresult = new BoundField();
-                //bresult.HeaderText = "结果";
-                //bresult.ReadOnly = false;
-                //grid[i].Columns.Add(bresult);
+                grid[i].DataBind();
             }
         }
 
         protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
             int index = (form1.Controls.IndexOf((DropDownList)sender) - 3) / 2 - 1;
             source[index].SelectCommand = "SELECT * FROM TESTITEM WHERE TYPE=" + Convert.ToInt32(droptype[index].SelectedValue);
-            grid[index].DataSource = source[index];
+            //grid[index].DataSource = source[index];
             grid[index].DataBind();
-            Response.Write(index);
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            string strcon = System.Configuration.ConfigurationManager.ConnectionStrings["HospitalData"].ConnectionString;
+            droptype[count] = new DropDownList();
+            grid[count] = new GridView();
 
-            //droptype = new DropDownList();
-            //form1.Controls.Add(droptype);
-           
-            
-            //GridView grid = new GridView();
-            //form1.Controls.Add(grid);
+            form1.Controls.Add(droptype[count]);
+            form1.Controls.Add(grid[count]);
 
-            droptype[count].Visible = true;
-            grid[count++].Visible = true;
-            
-            //TemplateField tresult = new TemplateField();
-            //tresult.HeaderText = "结果";
-            //tresult.ItemTemplate = TextBox as ITemplate;
+            droptype[count].SelectedIndexChanged += new EventHandler(DropDownList1_SelectedIndexChanged);
+            droptype[count].AutoPostBack = true;
+            droptype[count].DataSourceID = "SqlDataSource1";
+            droptype[count].DataValueField = "TYPE";
+            droptype[count].DataTextField = "NAME";
+            droptype[count].DataBind();
 
-            //grid.Columns.Add(tresult);
+            string[] array = { "TID" };
+            grid[count].DataKeyNames = array;
+            //SqlDataSource2.SelectCommand = "SELECT * FROM TESTITEM WHERE TYPE=" + Convert.ToInt32(droptype[i].SelectedValue);
+            source[count] = new SqlDataSource(strcon, "SELECT * FROM TESTITEM WHERE TYPE=" + Convert.ToInt32(droptype[count].SelectedValue));
 
+            grid[count].DataSource = source[count];
+            //grid[count].DataBind();
+            grid[count].AutoGenerateColumns = false;
+            grid[count].Columns.Clear();
 
-            //Response.Redirect("NewFollowUpTest.aspx");
+            BoundField bid = new BoundField();
+            bid.DataField = "TID";
+            bid.HeaderText = "No.";
+            grid[count].Columns.Add(bid);
+
+            BoundField bname = new BoundField();
+            bname.DataField = "NAME";
+            bname.HeaderText = "项目";
+            grid[count].Columns.Add(bname);
+
+            BoundField bunit = new BoundField();
+            bunit.DataField = "UNIT";
+            bunit.HeaderText = "单位";
+            grid[count].Columns.Add(bunit);
+
+            TemplateField bresult = new TemplateField();
+            TextBoxTemplate template = new TextBoxTemplate();
+            bresult.HeaderText = "结果";
+            bresult.ItemTemplate = template;
+            grid[count].Columns.Add(bresult);
+
+            grid[count++].DataBind();
+
+            SaveViewState();
         }
     }
 }
